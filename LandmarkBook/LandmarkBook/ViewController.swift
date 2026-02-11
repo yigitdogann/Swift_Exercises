2b537e7c13cbe2e915e48a8ef527bc7fb106cfa2
Yiğit Doğan <yigitdogan2k@gmail.com>
2026-02-11 20:48:38 +0300

    LandmarkBook Project created

---
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Yiğit Doğan on 11.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
